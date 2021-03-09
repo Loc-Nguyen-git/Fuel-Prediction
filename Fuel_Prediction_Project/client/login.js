b226e62564c384ce3513583884b35c80d69a07b9
locnguyen94 <77898734+locnguyen94@users.noreply.github.com>
2021-03-09 13:27:22 -0600

    create Postgres database and connect NodeJS backend to database in server folder

---
+++ Fuel_Prediction_Project/client/login.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+const bodyParser = require('body-parser');
+
+async function signin() {
+    
+}
---
+++ Fuel_Prediction_Project/server/database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE fuelproject
+
+CREATE TABLE User(
+    user_name varchar(255) PRIMARY KEY,
+    password varchar(255),
+    email varchar(255)
+);
