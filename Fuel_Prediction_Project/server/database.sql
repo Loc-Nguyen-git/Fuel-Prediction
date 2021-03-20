1b9a074d00fcbe734ed40a7bc0bcaa8179e51cf6
locnguyen94 <77898734+locnguyen94@users.noreply.github.com>
2021-03-20 14:28:22 -0500

    create database and create connection to backend

--- Fuel_Prediction_Project/server/database.sql
+++ Fuel_Prediction_Project/server/database.sql
@@ -1,6 +1,11 @@
+DROP DATABASE IF EXISTS fuelproject;
 CREATE DATABASE fuelproject;
 
-CREATE TABLE User(
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS profileInfo CASCADE;
+DROP TABLE IF EXISTS fuelquote CASCADE;
+
+CREATE TABLE Users(
     user_name varchar(255) PRIMARY KEY,
     password char(255),
     email varchar(255)
@@ -16,8 +21,16 @@ CREATE TABLE profileInfo(
     zip int
 );
 
-INSERT INTO User (
-    user_name,
-    password,
-    email)
-VALUES ('group18','cosc','cosc4354@gmail.com');
+CREATE TABLE fuelquote(
+    Gallons_Requested numeric(10,2) NOT NULL,
+	Delivery_Address char(255),
+	Delivery_Date timestamp ,
+	Suggested_Price numeric(10,2),
+	Total_Amount numeric(10,2)
+);
+
+INSERT INTO Users VALUES ('Moderna','cosc','cosc4354@gmail.com');
+INSERT INTO Users VALUES ('Pfizer','cosc','cosc4353@gmail.com');
+
+INSERT INTO profileinfo VALUES ('Moderna','Chad Raymor','6377 Instabull Dr',' ', 'Spring', 'TX', '77053');
+INSERT INTO profileinfo VALUES ('Pfizer','Loc Nguyen','6378 Mocha Ln',' ', 'Katy', 'TX', '77056');
--- Fuel_Prediction_Project/server/db.js
+++ Fuel_Prediction_Project/server/db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   host: 'localhost',
-  user: 'progres', // this is your postgreSQL or Ubuntu account --- change
+  user: 'postgres', // this is your postgreSQL or Ubuntu account --- change
   password: 'cosc4353', // this is your postgreSQL or Ubuntu account's password ---- change
   port: 5432,
-  database: 'fuelproject' // this is in the dabase.sql
+  database: 'fuelproject' // this is in the dabase.sql, run this in postgres user
 });
 
 module.exports = pool;
