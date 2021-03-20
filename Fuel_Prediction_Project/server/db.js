7631d2f9149adc9963d3098e1062565779939549
locnguyen94 <77898734+locnguyen94@users.noreply.github.com>
2021-03-20 13:54:26 -0500

    Assignment 4

--- Fuel_Prediction_Project/server/db.js
+++ Fuel_Prediction_Project/server/db.js
@@ -2,8 +2,8 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   host: 'localhost',
-  user: 'cosc0177', // this is your postgreSQL or Ubuntu account --- change
-  password: 'cosc4535', // this is your postgreSQL or Ubuntu account's password ---- change
+  user: 'progres', // this is your postgreSQL or Ubuntu account --- change
+  password: 'cosc4353', // this is your postgreSQL or Ubuntu account's password ---- change
   port: 5432,
   database: 'fuelproject' // this is in the dabase.sql
 });
